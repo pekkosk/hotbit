0ac5cde8e82b468f585eb739e13770ee9888e86c
pekkosk <pekkosk@fbeb3729-e554-0410-acff-8682e2a94fd5>
2008-10-03 07:14:43 +0000

    Wiki stuff

---
+++ trunk/hotbit/doc/examples/ground_state_calculation.py
@@ -0,0 +1,9 @@
+ # let us calculate Au dimer
+from ase import *
+from hotbit import Calculator
+
+calc=Calculator(SCC=True,width=0.05,txt='test.cal')
+Au2=Atoms('Au2',positions=[(0,0,0),(2.6,0,0)],cell=(10,10,10),pbc=False)
+Au2.center(vacuum=10)
+Au2.set_calculator(calc)
+print Au2.get_potential_energy()
