cc7c203da560ca80fc05a34971922327375859ec
vipemaki <vipemaki@fbeb3729-e554-0410-acff-8682e2a94fd5>
2009-02-18 12:05:54 +0000

    Use default element data for linear_response test.

--- hotbit/test/linear_response.py
+++ hotbit/test/linear_response.py
@@ -2,12 +2,14 @@ from ase import *
 from hotbit import Hotbit
 from hotbit.analysis import LinearResponse
 from ase.data.molecules import molecule
+from hotbit.test.misc import default_param
 
 atoms=Atoms('Na3',[(1.6964999745231999,0,0),(-1.6964999745231999,0,0),(0,2.9384240999630005,0)])
 atoms.center(vacuum=5)
 
+default_param['width'] = 0.0136
 
-calc=Hotbit(SCC=True,charge=1,width=0.0136)
+calc=Hotbit(SCC=True,charge=1,**default_param)
 atoms.set_calculator()
 calc.solve_ground_state(atoms)
 
