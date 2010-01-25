24ffe857fedacaec82fead6e33928dd74232de5c
vipemaki <vipemaki@fbeb3729-e554-0410-acff-8682e2a94fd5>
2010-01-25 08:42:29 +0000

    More C-stuff

--- trunk/hotbit/c/density_matrix.c
+++ trunk/hotbit/c/density_matrix.c
@@ -25,6 +25,7 @@ double complex *c_rhoc_(double complex *wf, double *occ, int norb, int nk) {
             }
         }
     }
+    // FIXME probably faster with different order of loops
     for (k=0; k<nk; k++) {
         for (i=0; i<norb; i++) {
             for (j=i; j<norb; j++) {
@@ -63,8 +64,65 @@ static PyObject *c_rhoc(PyObject *self, PyObject *args) {
     return Py_BuildValue("O", pao_rho);
 }
 
+double complex *c_rhoec_(double complex *wf, double *occ, double *e, int norb, int nk) {
+    int mx, i, j, k, n;
+    double complex *rho;
+
+    rho = (double complex*)calloc(nk*norb*norb, sizeof(double complex));
+
+    mx=-1;
+    for (i=norb-1; i>=0; i--) {
+        if (mx != -1) {
+            break;
+        }
+        for (k=0; k<nk; k++) {
+            if (occ[k*norb+i] > 1.0e-15) {
+                mx = i;
+                break;
+            }
+        }
+    }
+    // FIXME probably faster with different order of loops
+    for (k=0; k<nk; k++) {
+        for (i=0; i<norb; i++) {
+            for (j=i; j<norb; j++) {
+                for (n=0; n<mx+1; n++) {
+                    rho[k*norb*norb +i*norb +j] += e[k*norb +n]*occ[k*norb +n]*wf[k*norb*norb +n*norb +i]*conj(wf[k*norb*norb +n*norb +j]);
+                    rho[k*norb*norb+j*norb+i] = conj(rho[k*norb*norb+i*norb+j]);
+                }
+            }
+        }
+    }
+    return rho;
+}
+
+static PyObject *c_rhoec(PyObject *self, PyObject *args) {
+    int norb, nk;
+    npy_intp *wf_dim;
+    double *occ, *e;
+    double complex *wf, *rhoe;
+    PyObject *po_wf, *po_occ, *po_e;
+    PyArrayObject *pao_rhoe;
+
+    if (!PyArg_ParseTuple(args, "OOO", &po_wf, &po_occ, &po_e))
+        return NULL;
+
+    wf = (double complex*)(((PyArrayObject *)po_wf)->data);
+    occ = (double *)(((PyArrayObject *)po_occ)->data);
+    e = (double *)(((PyArrayObject *)po_e)->data);
+    wf_dim = (npy_intp *)(((PyArrayObject *)po_wf)->dimensions);
+
+    nk = wf_dim[0];
+    norb = wf_dim[1];
+    rhoe = c_rhoec_(wf, occ, e, norb, nk);
+
+    pao_rhoe = (PyArrayObject *)PyArray_SimpleNewFromData(3, wf_dim, NPY_CDOUBLE, rhoe);
+    return Py_BuildValue("O", pao_rhoe);
+}
+
 PyMethodDef methods[] = {
-    {"c_rhoc", c_rhoc, METH_VARARGS, "Solve the generalized eigenvalue equation"},
+    {"c_rhoc", c_rhoc, METH_VARARGS, "Calculate the complex density matrix."},
+    {"c_rhoec", c_rhoec, METH_VARARGS, "Calculate the energy-weighted complex density matrix."},
     {NULL, NULL, 0, NULL}
 };
 
--- trunk/hotbit/c/test_density_matrix.py
+++ trunk/hotbit/c/test_density_matrix.py
@@ -1,17 +1,33 @@
-from hotbit.fortran.misc import fortran_rhoc
-from density_matrix import c_rhoc
+from hotbit.fortran.misc import fortran_rhoc, fortran_rhoec
+from density_matrix import c_rhoc, c_rhoec
 import numpy as nu
 
-norb = 3
-nk = 1
+norb = 5
+nk = 3
 wf = nu.random.random((nk,norb,norb)) + 1j*nu.random.random((nk,norb,norb))
 occ = nu.zeros((nk, norb), nu.float)
 occ[:,0:norb/2+1] = 2.0
+e = nu.random.random((nk, norb))
 
 rho_f = fortran_rhoc(wf, occ, norb, nk)
 rho_c = c_rhoc(wf, occ)
 
-print rho_f
-print rho_c
-if nu.sum(abs(rho_f-rho_c)) < 1e-15:
+#print rho_f
+#print rho_c
+err = nu.sum(abs(rho_f-rho_c))
+if err < 1e-15:
     print "Complex density matrix OK"
+    print "    error: %f" % err
+    print ""
+
+rhoe_f = fortran_rhoec(wf, occ, e, norb, nk)
+rhoe_c = c_rhoec(wf, occ, e)
+#print rhoe_f
+#print rhoe_c
+err = nu.sum(abs(rhoe_f-rhoe_c))
+if err < 1e-15:
+    print "Energy-weighted complex density matrix OK"
+    print "    error: %f" % err
+    print ""
+
+
