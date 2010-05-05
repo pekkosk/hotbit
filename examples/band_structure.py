ebe9240354d2597dcdf541a5928240685fcb3894
pekkosk <pekkosk@fbeb3729-e554-0410-acff-8682e2a94fd5>
2010-05-05 06:19:56 +0000

    Changing keyword 'kappa' to 'rs', and updating band structure example.

--- examples/band_structure.py
+++ examples/band_structure.py
@@ -10,11 +10,14 @@ d = 4.08
 atoms = FaceCenteredCubic('Au', latticeconstant=d,
                           directions=((0,1,1),(1,0,1),(1,1,0)),
                           align=False)
-#view(atoms)
-a, b, c = atoms.get_cell()
-V = nu.dot(a, nu.cross(b,c))
+                          
+calc = Hotbit(SCC=False, kpts=(8,8,8), txt='bs.cal')
+atoms.set_calculator(calc)
+atoms.get_potential_energy()
 
 # reciprocal lattice vectors
+V = atoms.get_volume()
+a, b, c = atoms.get_cell()
 a_ = 2*pi/V*nu.cross(b, c)
 b_ = 2*pi/V*nu.cross(c, a)
 c_ = 2*pi/V*nu.cross(a, b)
@@ -25,16 +28,11 @@ gamma = nu.array( (0,0,0) )
 kpts, distances, label_points = interpolate_path((-a_/2, a_/2, gamma, -b_/2, b_/2, gamma, -c_/2, c_/2, gamma, (-a_-b_-c_)/2, (a_+b_+c_)/2), 500)
 labels = ["$-a$",'$a$','$\Gamma$','$-b$','$b$','$\Gamma$','$-c$','$c$','$\Gamma$','$-a-b-c$','$a+b+c$']
 
-# kappa=False, give explicit k-points instead of kappa-points
-calc = Hotbit(SCC=False, kpts=kpts, kappa=False)
-atoms.set_calculator(calc)
 
-atoms.get_potential_energy()
-
-kpts = calc.st.k
-eigs = calc.get_eigenvalues(atoms)
+eigs = calc.get_band_energies(kpts,shift=True,rs='k')
 e_min = nu.min(eigs)
 e_max = nu.max(eigs)
+
 
 # indices of the bands to plot [0,1,2,...]
 bands = range(len(eigs[0])) # plot all bands
