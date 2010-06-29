e65908e6ee2e8f4b6f27a431b891322d10c35cd3
lapastew <lapastew@fbeb3729-e554-0410-acff-8682e2a94fd5>
2010-06-29 20:05:50 +0000

    Removed build directory.

--- build/lib.linux-x86_64-2.6/box/wedgeatoms/__init__.py
+++
@@ -1,4 +0,0 @@
-from bendatoms import BendAtoms
-from wedgeatoms import WedgeAtoms
-from general_lj import GeneralizedLennardJones
-#from atoms_generalized import Atoms
--- build/lib.linux-x86_64-2.6/hotbit/parametrization/__init__.py
+++
@@ -1,4 +0,0 @@
-from atom import KSAllElectron
-from slako import SlaterKosterTable 
-from fitting import RepulsiveFitting
-from fitting import ParametrizationTest
