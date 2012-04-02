7d856b469a7c17fef32afafbc154803c5dabbc4c
lopez <lopez@fbeb3729-e554-0410-acff-8682e2a94fd5>
2012-04-02 08:43:30 +0000

    using ase.data.molecules instead of ase.molecules

--- hotbit/test/standard_set.py
+++ hotbit/test/standard_set.py
@@ -1,7 +1,7 @@
 from hotbit import Hotbit 
 #from hotbit import Calculator0
 from ase import *
-from ase.structure import molecule
+from ase.data.molecules import molecule
 from hotbit.test.misc import default_param
 import sys
 
